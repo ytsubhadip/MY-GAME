67ba8e1cba5629dfe0de1b91ecc24ea1bba180fb
ytsubhadip <159522165+ytsubhadip@users.noreply.github.com>
2025-05-06 13:51:05 +0000

    error solve

--- rock_paper_seaser.c
+++ rock_paper_seaser.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 int NumberGenerate(int n){
     srand(time(NULL));
     int num = (rand() % n) + 1; // Adding 1 to shift range from 0-(n-1) to 1-n
     return num;
 }
-int main()
-{
+
+int game(int computer){
+    
     int user ;
     printf("\nEnter (1. Rock 2.Paper 3.Seassor): ");
     scanf("%d",&user);
-    int computer = NumberGenerate(3);
     printf("computer : %d",computer);
     if(user == 1 && computer == 2){
         printf("\nResult: compuetr win\n");
@@ -29,16 +30,22 @@ int main()
         printf("\nResult: computer win\n");
     }
     else if(user == 3 && computer == 2){
-        printf("\nResult: user win\n");
+        printf("\nResult: You win\n");
     }
     else{
         printf("\nResult: draw\n");
     }
+    return 1;
+}
+int main()
+{
 
-    
- 
-   
-
+    int i=1;
+    while(i<=3){
+         int num = NumberGenerate(3);
+        game(num);
+        i++;
+    }
     
     return 0;
 }
